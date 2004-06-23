06eda4ee867867b9ddbf14216f87d49da6cdd589
Ryan Davis <ryand@zenspider.com>
2004-06-22 18:02:46 -0800

    Expanded expected value arrays so future diffs are worthwhile pair:eric
    [git-p4: depot-paths = "//src/ruby_to_c/dev/": change = 1523]

--- test_parse_tree.rb
+++ test_parse_tree.rb
@@ -9,21 +9,150 @@ class TestParseTree < Test::Unit::TestCase
   # TODO: need a test of interpolated strings
 
   @@missing = [nil]
-  @@empty = [:defn, "empty", [:scope, [:args]]]
-  @@stupid = [:defn, "stupid", [:scope, [:block, [:args], [:return, [:nil]]]]]
-  @@simple = [:defn, "simple", [:scope, [:block, [:args, "arg1"], [:fcall, "print", [:array, [:lvar, "arg1"]]], [:fcall, "puts", [:array, [:call, [:lit, 4], "+", [:array, [:lit, 2]]]]]]]]
-  @@global = [:defn, "global", [:scope, [:block, [:args], [:call, [:gvar, "$stderr"], "puts", [:array, [:str, "blah"]]]]]]
-  @@lasgn_call = [:defn, "lasgn_call", [:scope, [:block, [:args], [:lasgn, "c", [:call, [:lit, 2], "+", [:array, [:lit, 3]]]]]]]
-  @@conditional1 = [:defn, "conditional1", [:scope, [:block, [:args, "arg1"], [:if, [:call, [:lvar, "arg1"], "==", [:array, [:lit, 0]]], [:return, [:lit, 1]], nil]]]]
-  @@conditional2 = [:defn, "conditional2", [:scope, [:block, [:args, "arg1"], [:if, [:call, [:lvar, "arg1"], "==", [:array, [:lit, 0]]], nil, [:return, [:lit, 2]]]]]]
-  @@conditional3 = [:defn, "conditional3", [:scope, [:block, [:args, "arg1"], [:if, [:call, [:lvar, "arg1"], "==", [:array, [:lit, 0]]], [:return, [:lit, 3]], [:return, [:lit, 4]]]]]]
-  @@conditional4 = [:defn, "conditional4", [:scope, [:block, [:args, "arg1"], [:if, [:call, [:lvar, "arg1"], "==", [:array, [:lit, 0]]], [:return, [:lit, 2]], [:if, [:call, [:lvar, "arg1"], "<", [:array, [:lit, 0]]], [:return, [:lit, 3]], [:return, [:lit, 4]]]]]]]
-  @@iteration_body = [:scope, [:block, [:args], [:lasgn, "array", [:array, [:lit, 1], [:lit, 2], [:lit, 3]]], [:iter, [:call, [:lvar, "array"], "each"], [:dasgn_curr, "x"], [:fcall, "puts", [:array, [:dvar, "x"]]]]]]
+  @@empty = [:defn, "empty",
+    [:scope,
+      [:args]]]
+  @@stupid = [:defn, "stupid",
+    [:scope,
+      [:block,
+        [:args],
+        [:return, [:nil]]]]]
+  @@simple = [:defn, "simple",
+    [:scope,
+      [:block,
+        [:args, "arg1"],
+        [:fcall, "print",
+          [:array, [:lvar, "arg1"]]],
+        [:fcall, "puts",
+          [:array,
+            [:call,
+              [:lit, 4],
+              "+",
+              [:array, [:lit, 2]]]]]]]]
+  @@global = [:defn, "global",
+    [:scope,
+      [:block,
+        [:args],
+        [:call,
+          [:gvar, "$stderr"],
+          "puts",
+          [:array, [:str, "blah"]]]]]]
+  @@lasgn_call = [:defn, "lasgn_call",
+    [:scope,
+      [:block,
+        [:args],
+        [:lasgn, "c",
+          [:call,
+            [:lit, 2],
+            "+",
+            [:array, [:lit, 3]]]]]]]
+  @@conditional1 = [:defn, "conditional1",
+    [:scope,
+      [:block,
+        [:args, "arg1"],
+        [:if,
+          [:call,
+            [:lvar, "arg1"],
+            "==",
+            [:array, [:lit, 0]]],
+          [:return,
+            [:lit, 1]], nil]]]]
+  @@conditional2 = [:defn, "conditional2",
+    [:scope,
+      [:block,
+        [:args, "arg1"],
+        [:if,
+          [:call,
+            [:lvar, "arg1"],
+            "==",
+            [:array, [:lit, 0]]], nil,
+          [:return,
+            [:lit, 2]]]]]]
+  @@conditional3 = [:defn, "conditional3",
+    [:scope,
+      [:block,
+        [:args, "arg1"],
+        [:if,
+          [:call,
+            [:lvar, "arg1"],
+            "==",
+            [:array, [:lit, 0]]],
+          [:return,
+            [:lit, 3]],
+          [:return,
+            [:lit, 4]]]]]]
+  @@conditional4 = [:defn, "conditional4",
+    [:scope,
+      [:block,
+        [:args, "arg1"],
+        [:if,
+          [:call,
+            [:lvar, "arg1"],
+            "==",
+            [:array, [:lit, 0]]],
+          [:return, [:lit, 2]],
+          [:if,
+            [:call,
+              [:lvar, "arg1"],
+              "<",
+              [:array, [:lit, 0]]],
+            [:return, [:lit, 3]],
+            [:return, [:lit, 4]]]]]]]
+  @@iteration_body = [:scope,
+    [:block,
+      [:args],
+      [:lasgn, "array",
+        [:array, [:lit, 1], [:lit, 2], [:lit, 3]]],
+      [:iter,
+        [:call,
+          [:lvar, "array"], "each"],
+        [:dasgn_curr, "x"],
+        [:fcall, "puts", [:array, [:dvar, "x"]]]]]]
   @@iteration1 = [:defn, "iteration1", @@iteration_body]
   @@iteration2 = [:defn, "iteration2", @@iteration_body]
-  @@iteration3 = [:defn, "iteration3", [:scope, [:block, [:args], [:lasgn, "array1", [:array, [:lit, 1], [:lit, 2], [:lit, 3]]], [:lasgn, "array2", [:array, [:lit, 4], [:lit, 5], [:lit, 6], [:lit, 7]]], [:iter, [:call, [:lvar, "array1"], "each"], [:dasgn_curr, "x"], [:iter, [:call, [:lvar, "array2"], "each"], [:dasgn_curr, "y"], [:block, [:fcall, "puts", [:array, [:dvar, "x"]]], [:fcall, "puts", [:array, [:dvar, "y"]]]]]]]]]
-  @@multi_args = [:defn, "multi_args", [:scope, [:block, [:args, "arg1", "arg2"], [:fcall, "puts", [:array, [:call, [:lvar, "arg1"], "*", [:array, [:lvar, "arg2"]]]]], [:return, [:str, "foo"]]]]]
-  @@bools = [:defn, "bools", [:scope, [:block, [:args, "arg1"], [:if, [:call, [:lvar, "arg1"], "nil?"], [:return, [:false]], [:return, [:true]]]]]]
+  @@iteration3 = [:defn, "iteration3",
+    [:scope,
+      [:block,
+        [:args],
+        [:lasgn, "array1",
+          [:array, [:lit, 1], [:lit, 2], [:lit, 3]]],
+        [:lasgn, "array2",
+          [:array, [:lit, 4], [:lit, 5], [:lit, 6], [:lit, 7]]],
+        [:iter,
+          [:call,
+            [:lvar, "array1"], "each"],
+          [:dasgn_curr, "x"],
+          [:iter,
+            [:call,
+              [:lvar, "array2"], "each"],
+            [:dasgn_curr, "y"],
+            [:block,
+              [:fcall, "puts",
+                [:array, [:dvar, "x"]]],
+              [:fcall, "puts",
+                [:array, [:dvar, "y"]]]]]]]]]
+  @@multi_args = [:defn, "multi_args",
+    [:scope,
+      [:block,
+        [:args, "arg1", "arg2"],
+        [:fcall, "puts",
+          [:array, [:call,
+              [:lvar, "arg1"],
+              "*",
+              [:array, [:lvar, "arg2"]]]]],
+        [:return,
+          [:str, "foo"]]]]]
+  @@bools = [:defn, "bools",
+    [:scope,
+      [:block,
+        [:args, "arg1"],
+        [:if,
+          [:call,
+            [:lvar, "arg1"], "nil?"],
+          [:return,
+            [:false]],
+          [:return,
+            [:true]]]]]]
 
   @@__all = []
 
