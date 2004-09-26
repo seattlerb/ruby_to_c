056f1f3de399dcc43c02b0816a59e5dff39edbd4
Ryan Davis <ryand@zenspider.com>
2004-09-26 15:06:47 -0800

    Added a composite sexp processor and tests to R2C.  Extended ZenTest to work on standard input, added better method name mapping, and extended to handle <<.
    [git-p4: depot-paths = "//src/ruby_to_c/dev/": change = 1603]

---
+++ composite_sexp_processor.rb
@@ -0,0 +1,23 @@
+require 'sexp_processor'
+
+class CompositeSexpProcessor < SexpProcessor
+
+  attr_reader :processors if $TESTING
+
+  def initialize(*processors)
+    super
+    @processors = []
+  end
+
+  def <<(processor)
+    raise ArgumentError, "Can only add sexp processors" unless SexpProcessor === processor
+    @processors << processor
+  end
+
+  def process(exp)
+    @processors.each do |processor|
+      exp = processor.process(exp)
+    end
+    exp
+  end
+end
